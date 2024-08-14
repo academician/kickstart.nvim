056bd4ccd7083467e44d26cd874a0e73d1149e59
Daniel Waechter <daniel.waechter@wolfjawstudios.com>
2024-08-14 11:03:45 -0400

    which-key config

--- init.lua
+++ init.lua
@@ -345,12 +345,20 @@ require('lazy').setup({
           F12 = '<F12>',
         },
       },
-
-      -- Document existing key chains
+      preset = 'helix',
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      },
+    },
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
       },
     },
   },
